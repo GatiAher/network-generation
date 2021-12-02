c536f7e7d9daf52104d12549f69d08422955b702
Aydin O'Leary <aoleary@olin.edu>
2021-12-02 14:56:01 -0500

    Display ring lattice

--- watts_strogatz.py
+++ watts_strogatz.py
@@ -1,14 +1,17 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import math
 
-N = 7
-K = 4
+# parameters
+N = 15
+K = 8
 P = 0.2
 
 halfK = math.floor(K/2)
 nodes = range(1,N+1)
-ring_lattice = nx.Graph()
 
+# create ring lattice
+ring_lattice = nx.Graph()
 ring_lattice.add_nodes_from(nodes)
 
 for i, u in enumerate(ring_lattice.nodes):
@@ -16,4 +19,8 @@ for i, u in enumerate(ring_lattice.nodes):
         v = (j % N) + 1
         ring_lattice.add_edge(u, v)
 
-print(ring_lattice)
+nx.draw_circular(ring_lattice)
+plt.show()
+
+# randomize ring lattice to create Watts-Strogatz
+ws = nx.Graph(ring_lattice)
